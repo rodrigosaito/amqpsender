8bcebd90563520f8d954655d43aa18d97da325f8
Rodrigo Saito <rodrigo.saito@gmail.com>
2015-11-13 23:12:41 -0200

    Add option to send headers

--- main.go
+++ main.go
@@ -14,11 +14,12 @@ import (
 const VERSION = "0.0.3"
 
 func main() {
-	var amqpUrl, exchange, routingKey string
+	var amqpUrl, exchange, routingKey, headers string
 	var version bool
 	flag.StringVar(&amqpUrl, "amqp-url", "amqp://guest:guest@localhost:5672", "rabbitmq url to send the messages to")
 	flag.StringVar(&exchange, "exchange", "", "rabbitmq exchange to be used")
 	flag.StringVar(&routingKey, "routing-key", "", "routing key to be used")
+	flag.StringVar(&headers, "headers", "", "headers to be sent format: name1:value1,name2:value2")
 	flag.BoolVar(&version, "v", false, "show current version")
 	flag.Parse()
 
@@ -44,7 +45,7 @@ func main() {
 	log.Printf("Using amqp-url: [%v], exchange: [%v], routing-key: [%v]", amqpUrl, exchange, routingKey)
 
 	if len(args) > 0 {
-		if err = publish(channel, exchange, routingKey, args[0]); err != nil {
+		if err = publish(channel, exchange, routingKey, parseHeaders(headers), args[0]); err != nil {
 			panic(err)
 		}
 	} else {
@@ -56,14 +57,27 @@ func main() {
 				break
 			}
 
-			if err = publish(channel, exchange, routingKey, strings.TrimSpace(line)); err != nil {
+			if err = publish(channel, exchange, routingKey, parseHeaders(headers), strings.TrimSpace(line)); err != nil {
 				panic(err)
 			}
 		}
 	}
 }
 
-func publish(channel *amqp.Channel, exchange, routingKey, message string) error {
+func parseHeaders(h string) map[string]interface{} {
+	headers := map[string]interface{}{}
+
+	values := strings.Split(h, ",")
+	for _, v := range values {
+		name := strings.Split(v, ":")[0]
+		value := strings.Split(v, ":")[1]
+		headers[name] = value
+	}
+
+	return headers
+}
+
+func publish(channel *amqp.Channel, exchange, routingKey string, headers map[string]interface{}, message string) error {
 	log.Printf("Publishing message: [%v]", message)
 	err := channel.Publish(
 		exchange,   // publish to an exchange
@@ -71,7 +85,8 @@ func publish(channel *amqp.Channel, exchange, routingKey, message string) error 
 		false,      // mandatory
 		false,      // immediate
 		amqp.Publishing{
-			Body: []byte(message),
+			Body:    []byte(message),
+			Headers: headers,
 		},
 	)
 
